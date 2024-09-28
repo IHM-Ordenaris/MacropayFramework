8488816b65a42105d4e619ff7cc218796c87dc4f
Ignacio H <ignacio.hernandez@ordenaris.com>
2024-09-27 18:58:30 -0600

    TA # New Struct Swift Package

--- MySwiftPackage/Package.swift
+++ MySwiftPackage/Package.swift
@@ -3,26 +3,29 @@
 
 import PackageDescription
 
+import PackageDescription
+
 let package = Package(
-    name: "MacroPayOrdenarisSDK",
+    name: "MySwiftPackage",
     platforms: [
         .iOS(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MacroPayOrdenarisSDK",
-            targets: ["MacroPayOrdenarisSDK"]),
+            name: "MyLibrary",
+            targets: ["MyLibrary"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MyLibrary",
+            path: "Sources/MyLibrary"
+        ),
         .binaryTarget(
             name: "MacropayFramework",
             path: "./FrameworkExample.xcframework"
         ),
-        .target(
-            name: "MacroPayOrdenarisSDK"),
-
     ]
 )
--- MySwiftPackage/README.md
+++ MySwiftPackage/README.md
@@ -5,4 +5,4 @@
 To install this package using Swift Package Manager, add the following line to your dependencies:
 
 ```swift
-.package(url: "https://github.com/IHM-Ordenaris/MacropayFramework.git", from: "1.0.0")
+.package(url: "https://github.com/IHM-Ordenaris/MacropayFramework.git", from: "1.0.1")
